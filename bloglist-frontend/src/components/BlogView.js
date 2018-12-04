55e2c73a1761468dce311e0f1f4f95f8d3b5b917
Heli Hyvättinen <heli.hyvattinen@netti.fi>
2018-12-04 22:58:13 +0200

    Yksittäisen blogin näkymä toimii vaikka lisännyttä käyttäjää ei ole
    Delete-nappi lisätty, mutta näkyy vain jos blogilla ei käyttäjää

--- bloglist-frontend/src/components/BlogView.js
+++ bloglist-frontend/src/components/BlogView.js
@@ -1,10 +1,42 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { addLike } from './../reducers/blogReducer'
+import PropTypes from 'prop-types'
 
-const handleAddLike = (blog) => () =>  { 
+const handleAddLike = (blog) => () => addLike(blog)
 
-    addLike(blog)
+const DeleteButton = ({onClick,blog,user}) => {
+
+    if (blog.user && user && blog.user.username !== user.username) {
+      return null
+    } else {
+      return (<button type="button" onClick={onClick} >delete</button>)
+    }
+  }
+  
+  DeleteButton.propTypes = {
+    onClick: PropTypes.func.isRequired,
+    blog: PropTypes.shape({
+      user: PropTypes.shape({
+        username: PropTypes.string
+      })
+    }).isRequired,
+    user: PropTypes.shape({
+      username: PropTypes.string
+    }).isRequired
+  }
+  
+
+  const handleDelete = () => {
+
+
+    if (window.confirm(`Delete '${this.state.blog.title}' by ${this.state.blog.author}?`)) {
+    
+      this.props.deleteBlog(this.state.blog._id)
+//          this.props.notify(`the blog '${blog.title}' by ${blog.author} deleted`, false, 5)
+
+//      this.props.notify(`deleting the blog '${blog.title}' by ${blog.author} failed: ${exception}`, true, 5)
+}
 }
 
 const BlogView = (props) => {
@@ -17,12 +49,15 @@ const BlogView = (props) => {
         return null
     }
 
+    const addedByText = blog.user ? "added by ".concat(blog.user.username) : ""
+
     return (
         <div>
             <h2>The {blog.title} {blog.author} </h2>
             <a href={blog.url}>{blog.url}</a><br/>
-            {blog.likes} likes <br/>
-            added by {blog.user.username} <button type="button" onClick={handleAddLike(blog)}>like</button><br/>
+            {blog.likes} likes <button type="button" onClick={handleAddLike(blog)}>like</button><br/>
+            {addedByText} <br/>
+            <DeleteButton onClick={handleDelete} blog={blog} user={props.currentUser}/>
 
         </div>
     )
@@ -30,7 +65,8 @@ const BlogView = (props) => {
 
 const mapStateToProps = (state) => {
     return {
-        blogs: state.blogs
+        blogs: state.blogs,
+        currentUser: state.user.username
     }
 } 
 
